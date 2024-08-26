666684e53560e021dfe1ba0b7679df6d921cef2e
Colm Moore <colm.moore3@gmail.com>
2024-08-26 19:35:28 +1000

    Worked on ways to access the AI - Added route for asking for questions (Still plenty to do) - Added some validation to the requests. - Modified AI interface for now

--- src/routes/questions/questions.dto.ts
+++ src/routes/questions/questions.dto.ts
@@ -16,3 +16,15 @@ export class QuestionDTO {
     file_id!: string;
 
 };
+
+export class QuestionRequestDTO {
+
+    @IsString({message: "file must be a string."})
+    file!: string;
+
+    @IsInt({message: "questions_count is required to be an int"})
+    @Max(6)
+    @Min(1)
+    question_count!: number;
+
+}
