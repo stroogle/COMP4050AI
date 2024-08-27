154fa4f3db3856c6f67bf73443919123af96b85a
Colm Moore <57727110+stroogle@users.noreply.github.com>
2024-08-27 13:38:06 +1000

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    testMatch: ["<rootDir>/src/**/*.test.ts"],
-};
-exports.default = config;
-//# sourceMappingURL=jest.config.js.map
