b79a43f07a84e3d81b783bfd264fc8729e625b70
Colm Moore <contact@colm.tech>
2024-09-02 21:37:04 +1000

    Beginning to turn innto library - Added interface for AiService - Created Mock implementation of AiService - Created AiFactory to determine if the user should use the Mock or Real implementation - Removed unnecessary files

---
+++ src/SarapeAi.ts
@@ -0,0 +1,29 @@
+import { Path } from "glob";
+import { AiService, Question } from "./AiService";
+
+
+export class SarapeAi implements AiService{
+
+    pdf_dir: string;
+    question_dir: string;
+    api_key: string;
+
+    constructor(
+        pdf_dir: string,
+        question_dir: string,
+        api_key: string
+    ) {
+        this.pdf_dir = pdf_dir;
+        this.question_dir = question_dir;
+        this.api_key = api_key;
+    }
+    
+    generateQuestions(pdf_name: string): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
+
+    getQuestions(questions_file: string): Promise<Array<Question>> {
+        throw new Error("Method not implemented.");
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-import express from "express";
 import * as dotenv from "dotenv";
-import question_routes from "./routes/questions"
 
 dotenv.config();
-
-const app = express();
-
-app.use(express.json())
-
-app.get("/", (req, res) => {
-    res.sendStatus(200);
-})
-
-app.use("/questions", question_routes);
-
-app.listen(8000);
